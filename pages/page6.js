d8e9eb76aee865372869991188447a0da86ed3b2
Marcoguzman211 <guzmanescobar3@gmail.com>
2023-01-05 14:30:04 +0100

    Modification components/page6, modèle d'une requete vers Wordpress pour récupérer les posts

--- pages/page6.js
+++ pages/page6.js
@@ -1,63 +1,32 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Layout.module.css";
-import { useEffect, useState } from "react";
 import Link from "next/link";
-import Nav from "../components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
+function page6( { posts }) {
+    posts.forEach(post => console.log(post));
+  return (
+    <>
+        <h1>Page 6</h1>
+        <ul>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Link href="/blog/[id]">
+                        <h1>{post.title.rendered}</h1>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </>
+  )
+}
 
-// SSR Server Side Rendering
+export default page6;
 
-// Next récupère les données en amont (précharge) et donne le rendu
-export const getStaticProps = async () => {
-    const posts = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_limit=5`
-    ).then((r) => r.json());
-    return {
-        props: {
-            posts,
-            date: new Date().toString(),
-        },
-        revalidate: 5,
-    };
-};
-
-export default function Page5({ posts, date }) {
-    // const [count, setCount] = useState(0);
-    // useEffect(() => {
-    //   const timer = setInterval(() => setCount((n) => n + 1), 1000);
-    //   return () => clearInterval(timer);
-    // });
-    console.log(posts);
-    return (
-        <>
-            <Head>
-                <title>My first blog 6</title>
-            </Head>
-            <h1>Page 6 avec approche statique incrémentale (approche hybride)</h1>
-            <br />
-            <h2>
-                {/* Compteur: {count} - {date} */}
-                {date}
-            </h2>
-            <br />
-            {/* Nav component with Nav.module.css style */}
-            <Nav />
-            <br />
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link href={`/blog/${post.id}`}>
-                            <h3>
-                                {post.id}/ {post.title}
-                            </h3>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <br />
-        </>
-    );
+export async function getStaticProps() {
+    //URL de Wordpress.
+  const response = await fetch('https://api.limonecat.com/wp-json/wp/v2/posts');
+  const posts = await response.json()
+  return {
+    props: {
+      posts,
+    },
+  };
 }
