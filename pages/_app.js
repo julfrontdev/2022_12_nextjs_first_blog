6fd513013a5a31a990bc942d67dc15558e1ed308
Marcoguzman211 <guzmanescobar3@gmail.com>
2023-01-11 14:21:37 +0100

    Nouvel deploiement de l'api avec le nouveau nom de domaine et changements sur apollo-client

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
+//ApolloClient
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo-client';
+
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>My first blog</title>
-        <meta name="My first blog" content="My first blog with Next.js" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+      <ApolloProvider client={client}>
+          <Layout>
+              <Head>
+                  <title>My first blog</title>
+                  <meta name="My first blog" content="My first blog with Next.js" />
+              </Head>
+              <Component {...pageProps} />
+          </Layout>
+      </ApolloProvider>
   );
 }
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,25 +1,50 @@
 import { useRouter } from 'next/router';
-const getPost = async (id) => {
+import { gql,useQuery } from "@apollo/client";
+import client  from "../../apollo-client"
+
+/*const getPost = async (id) => {
   const res = await fetch(`https://api.limonecat.com/wp-json/wp/v2/posts/${id}`);
   return res.json();
-}
+}*/
+
 
 const Post = (props) => {
-  const router = useRouter();
+  /*const router = useRouter();
   const { id } = router.query;
-  const { title } = props.post;
-
+  const { title } = props.post;*/
+console.log(props);
   return (
       <div>
-        <h1>{title.rendered}</h1>
+        <h1>{title}</h1>
       </div>
   );
 }
 
-Post.getInitialProps = async (ctx) => {
+/*Post.getInitialProps = async (ctx) => {
   const { id } = ctx.query;
   const post = await getPost(id);
   return { post };
+}*/
+
+export async function getStaticProps(ctx) {
+    const postID = ctx.params.id;
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data } = useQuery(
+        gql`
+            query($id: ID!) {
+                post(id: $id, idType: DATABASE_ID) {
+                    title
+                    content
+                }
+            }
+        `,
+        { variables: { id: postID } }
+    );
+    return {
+        props: {
+            data,
+        },
+    };
 }
 
 export default Post;
