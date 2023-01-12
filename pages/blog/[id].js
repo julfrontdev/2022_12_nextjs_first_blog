d5079c7ee510458aebec9eb0d152c8ce8998f248
Julien Verley <mjules@gmail.com>
2023-01-12 10:36:12 +0100

    merge de la branche dev

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,57 +1,54 @@
 import { gql } from "@apollo/client";
-import client  from "../../apollo-client"
-
+import client from "../../apollo-client";
 
 export async function getStaticPaths() {
-// Fetch the IDs of all posts from the server
-    const { data } = await client.query({
-        query: gql`
-            query {
-                posts {
-                    edges {
-                        node {
-                            id
-                        }
-                    }
-                }
+  // Fetch the IDs of all posts from the server
+  const { data } = await client.query({
+    query: gql`
+      query {
+        posts {
+          edges {
+            node {
+              id
             }
-        `,
-    });
-// Generate a path for each post ID
-    const paths = data.posts.edges.map((post) => ({
-        params: { id: post.node.id.toString() },
-    }));
-    return { paths, fallback: true };
+          }
+        }
+      }
+    `,
+  });
+  // Generate a path for each post ID
+  const paths = data.posts.edges.map((post) => ({
+    params: { id: post.node.id.toString() },
+  }));
+  return { paths, fallback: true };
 }
 
-
-
 export async function getStaticProps({ params }) {
-    const { data } = await client.query({
-        query: gql`
-            query($id: ID!) {
-                post(id: $id) {
-                    title
-                    content
-                }
-            }
-        `,
-        variables: { id: params.id },
-    });
-    return {
-        props: {
-            data,
-        },
-    };
+  const { data } = await client.query({
+    query: gql`
+      query ($id: ID!) {
+        post(id: $id) {
+          title
+          content
+        }
+      }
+    `,
+    variables: { id: params.id },
+  });
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
 const Post = ({ data }) => {
-    //Ici, il faut traiter {data} pour afficher le contenu du post.
-    return (
-        <div>
-            <h1>{data.post.title}</h1>
-            <p>{data.post.content}</p>
-        </div>
-    );
+  //Ici, il faut traiter {data} pour afficher le contenu du post.
+  return (
+    <div>
+      <h1>{data.post.title}</h1>
+      <p>{data.post.content}</p>
+    </div>
+  );
 };
 export default Post;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,51 +1,51 @@
 import Link from "next/link";
 import { gql } from "@apollo/client";
-import client  from "../../apollo-client"
+import client from "../../apollo-client";
 
-function blog( { data }) {
-    const posts = data.posts.edges.map((post) => post.node);
-    return (
-        <>
-            <h1>Blog alimenté par Wordpress</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link href="/blog/[id]" as={`/blog/${post.id}`}>
-                            <h1>{post.title}</h1>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+function blog({ data }) {
+  const posts = data.posts.edges.map((post) => post.node);
+  return (
+    <>
+      <h1>Blog alimenté par Wordpress</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href="/blog/[id]" as={`/blog/${post.id}`}>
+              <h1>{post.title}</h1>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default blog;
 
 export async function getStaticProps() {
-    const { data } = await client.query({
-        query: gql`
-            query {
-                posts {
-                    edges {
-                        node {
-                            id
-                            title
-                            content
-                            date
-                        }
-                    }
-                }
+  const { data } = await client.query({
+    query: gql`
+      query {
+        posts {
+          edges {
+            node {
+              id
+              title
+              content
+              date
             }
-        `,
-    });
+          }
+        }
+      }
+    `,
+  });
 
-    //URL de Wordpress.
-   /* const response = await fetch('https://api.limonecat.com/wp-json/wp/v2/posts');
+  //URL de Wordpress.
+  /* const response = await fetch('https://api.limonecat.com/wp-json/wp/v2/posts');
     const posts = await response.json()*/
-    return {
-        props: {
-            data,
-        },
-    };
+  return {
+    props: {
+      data,
+    },
+  };
 }
